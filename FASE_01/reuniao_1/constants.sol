4fa8ee57a5f41df8e38dc89a494cb7f86fc6eac5
valter <valterlobo@gmail.com>
2022-04-30 19:39:51 -0300

    fix:deleted files

--- FASE_01/reuniao_1/constants.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
-
-contract Constants {
-    // convenção de codificação para variáveis constantes em maiúsculas
-    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
-    uint256 public constant MY_UINT = 123;
-}
